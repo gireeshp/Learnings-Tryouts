5d31290659e307e00b9bcccc4d124db26c83687a
Gireesh Puthumana <gireesh.puthumana@gmail.com>
2020-01-19 10:16:16 +0530

    Flutter share final push

---
+++ flutter/udemy/fluttershare/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/gireesh/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Gireesh/Personal/Learning/Learnings-Tryouts/flutter/udemy/fluttershare"
+export "FLUTTER_TARGET=/Gireesh/Personal/Learning/Learnings-Tryouts/flutter/udemy/fluttershare/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/gireesh/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
